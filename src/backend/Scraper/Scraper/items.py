2d0516a8b0f77e4268c8af781b17d0d3fe711a51
Vid Pesko <vid@pesko.si>
2025-03-30 14:15:25 +0200

    Finished basic scraper

--- src/backend/Scraper/Scraper/items.py
+++ src/backend/Scraper/Scraper/items.py
@@ -3,10 +3,19 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+import re
 from datetime import datetime
 from dataclasses import field, dataclass
 
-import scrapy
+
+from itemloaders.processors import TakeFirst, MapCompose, Join, Identity
+from scrapy.loader import ItemLoader
+
+
+VALUES_TRANSLATION_TABLE = {
+    "Preverjanje znanja vožnje": "driving",
+    "Preverjanje znanja teorije": "theory",
+}
 
 
 @dataclass
@@ -18,3 +27,22 @@ class Slot:
     duration: int | None = field(default=None)
     location_district: str | None = field(default=None)
     location: str | None = field(default=None)
+
+
+class SlotLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+    date_in = MapCompose(lambda x: datetime.strptime(x, "%d. %m. %Y %H:%M"))
+
+    spots_left_in = MapCompose(
+        lambda x: int(re.search(r"Še\s*(\d+)\s*prosto mesto", x).group(1))
+    )
+
+    exam_type_in = MapCompose(lambda x: VALUES_TRANSLATION_TABLE.get(x, x))
+
+    categories_in = MapCompose(lambda x: x.replace(",", ""))
+    categories_out = Identity()
+
+    duration_in = MapCompose(lambda x: int(re.search(r"\(\s*trajanje\s*(\d+)\s*minut\s*\)", x).group(1)))
+
+    location_in = MapCompose(lambda x: x.removeprefix(", "))
--- src/backend/Scraper/Scraper/spiders/avp_scraper.py
+++ src/backend/Scraper/Scraper/spiders/avp_scraper.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import scrapy
 
-from ..items import Slot
+from ..items import Slot, SlotLoader
 
 
 class AvpSpider(scrapy.Spider):
@@ -14,9 +14,25 @@ class AvpSpider(scrapy.Spider):
 
     def parse(self, response):
         for event in response.css(".dogodek"):
-            slot = Slot()
+            slot = SlotLoader(item=Slot(), response=response, selector=event)
             # Date
+            start_time = event.xpath(".//div[contains(text(), 'Začetek ob')]//span/text()").get()  # Start time
             date = event.css(".calendarBox::attr('aria-label')").get()
-            print()
+            slot.add_value("date", f"{date} {start_time}")
+            # Spots
+            slot.add_css("spots_left", ".contentOpomnik > .lessImportant::text")
+            # Exam type
+            slot.add_css("exam_type", ".contentOpomnik > div:nth-of-type(2)::text")
+            # Categories
+            slot.add_xpath("categories", ".//div[contains(text(), 'Kategorije:')]//span/text()")
+            # Duration
+            slot.add_xpath(
+                "duration",
+                ".//div[contains(text(), 'Začetek ob')]/text()[last()]",
+            )
+            # Location
+            slot.add_css("location_district", ".upperOpomnikDiv > span::text")
+            slot.add_css("location", ".upperOpomnikDiv > span:nth-of-type(2)::text")
 
-            # yield None
+            slot_item = slot.load_item()
+            yield slot_item
