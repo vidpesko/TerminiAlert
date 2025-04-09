677897d6914af592e85068eca5a2ef7ed8a8d5a5
Vid Pesko <vid@pesko.si>
2025-04-09 17:15:27 +0200

    Changed navbar btns text sizes

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -31,7 +31,7 @@
                     Prijava
                 </a>
                 {:else}
-                <a href="/ustvari" type="button" class="hidden sm:inline-flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                <a href="/ustvari" type="button" class="hidden sm:inline-flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-3 py-1.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
                     <svg
                         aria-hidden="true"
                         class="mr-1 -ml-1 w-5 h-5"
