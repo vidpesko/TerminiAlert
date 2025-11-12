d5f7bd2bb2831908a66be608199395ab685ed1b9
Vid Ubuntu <vid@pesko.si>
2025-11-12 02:36:25 +0100

    Added PM2 ecosystem file

---
+++ src/frontend/ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [
+    {
+      name: "TerminiAlert-Frontend",
+      script: 'build/index.js',
+      watch: '.',
+      env: {
+        HTTPS_ENABLED: "false",
+        DOMAIN: "termini.pesko.si",
+        PORT: "3003",
+        HOST: "127.0.0.1",
+        ORIGIN: "http://termini.pesko.si"
+      }
+    }
+  ],
+};
